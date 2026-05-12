8180b100a2b24c116b0b9e2a1966c5f22de18e83
Trailblazer Vivek <trailblazervivek50@gmail.com>
2026-05-12 12:11:36 +0530

    question 33 with both approach

--- src/SearchInRotatedArray.java
+++ src/SearchInRotatedArray.java
@@ -1,48 +1,79 @@
 import java.util.Arrays;
-class Rev{
-    public static void reverse(int[]nums,int i,int j){
-        while (i<j){
-            int temp = nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
-            i++;
-            j--;
-        }
-    }
-}
+class Bruteforce {
 
+    public static int search(int[] nums, int target) {
 
-public class SearchInRotatedArray {
-    public static void main(String[] args) {
-        int []nums = {4,5,6,7,0,1,2};
+        int peak = -1;
 
-        int k=0;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]>nums[i+1]){
-                k=i;
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] > nums[i + 1]){
+                peak = i;
             }
         }
-        Rev.reverse(nums,0,k);Rev.reverse(nums,k+1,nums.length-1);Rev.reverse(nums,0, nums.length-1);
 
-//        System.out.println(Arrays.toString(nums));
-        System.out.println(BinarySearch(nums,0));
-
-//        System.out.println(nums);
-
+        if(target >= nums[0]){
+            return binarySearch(nums, target, 0, peak);
+        }
+        else{
+            return binarySearch(nums, target, peak + 1, nums.length - 1);
+        }
     }
-    public static int BinarySearch(int[]nums,int target){
-        int s = 0;
-        int e = nums.length-1;
-        while (s<=e){
-            int mid = s+(e-s)/2;
+
+    static int binarySearch(int[] nums, int target, int start, int end){
+
+        while(start <= end){
+
+            int mid = start + (end - start) / 2;
+
             if(target == nums[mid]){
                 return mid;
-            }else if(target>nums[mid]){
-                s=mid+1;
+            }
+            else if(target > nums[mid]){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
+class Optimal {
+    public static int search(int[] nums, int target) {
+        int start =0;
+        int end = nums.length-1;
+
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(target == nums[mid]){
+                return mid;
             }else{
-                e=mid-1;
+                if(nums[start]<=nums[mid]){
+                    if(target>=nums[start] && target<nums[mid]){
+                        end = mid -1;
+                    }else{
+                        start = mid+1;
+                    }
+                }else{
+                    if(target>nums[mid] && target<=nums[end]){
+                        start = mid+1;
+                    }else{
+                        end = mid-1;
+                    }
+                }
             }
         }
         return -1;
+    }
+}
+class SearchInRotatedArray
+{
+    public static void main(String[] args) {
+        int [] arr = {4,5,6,7,0,1,2};
+        int ans1 = Bruteforce.search(arr,0);
+        int ans2 = Optimal.search(arr,0);
+        System.out.println(ans1+"this is brute force with o(n+logn)complexity");
+        System.out.println(ans2+"this is optimal with O(logn) complexity");
     }
 }
