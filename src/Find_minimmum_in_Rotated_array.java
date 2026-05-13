fc6988d8f56ec1b31e1e297fc494a0b406b05d87
Trailblazer Vivek <trailblazervivek50@gmail.com>
2026-05-13 15:22:48 +0530

    Both types of approaches of Question 153

--- src/Find_minimmum_in_Rotated_array.java
+++ src/Find_minimmum_in_Rotated_array.java
@@ -8,11 +8,30 @@ class Brute{
         return arr[0];
     }
 }
+class Optimal1 {
+    public static int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(start<end && nums[mid]>nums[mid+1]){
+                return nums[mid+1];
+            }else if(nums[start]>nums[mid]){
+                end = mid;
+            }else{
+                start= mid+1;
+            }
+        }
+        return nums[0];
+    }
+}
 
 
 public class Find_minimmum_in_Rotated_array {
     public static void main(String[] args) {
         int []nums = {3,4,5,1,2};
-        System.out.println(Brute.findMin(nums));
+        System.out.println(Brute.findMin(nums)+" O(n)");
+        System.out.println(Optimal1.findMin(nums)+" O(logn)");
     }
 }
